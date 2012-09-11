69b08c30234f5ce0fd2791ba733eb2992e8c2bad
Benjamin Klein <ben@silver-chalice.com>
2012-09-11 15:20:53 -0500

    use CocoaConf DB

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,43 +1,65 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
+    driverClassName = "org.hsqldb.jdbcDriver"
     username = "sa"
     password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.use_query_cache = true
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            pooled = true
+            driverClassName = "com.mysql.jdbc.Driver"
+            username = "cocoaconf"
+            password = "cc5551212"
+            dbCreate = "update"
+            url = "jdbc:mysql://207.223.253.64:3306/cocoaDb"
+            dialect=org.hibernate.dialect.MySQLInnoDBDialect
+            properties {
+                validationQuery="select 1"
+                testWhileIdle=true
+                timeBetweenEvictionRunsMillis=60000
+            }
+
         }
     }
     test {
         dataSource {
+            pooled = true
+            driverClassName = "com.mysql.jdbc.Driver"
+            username = "cocoaconf"
+            password = "cc5551212"
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://207.223.253.64:3306/cocoaDb"
+            dialect=org.hibernate.dialect.MySQLInnoDBDialect
+            properties {
+                validationQuery="select 1"
+                testWhileIdle=true
+                timeBetweenEvictionRunsMillis=60000
+            }
+
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
             pooled = true
+            driverClassName = "com.mysql.jdbc.Driver"
+            username = "cocoaconf"
+            password = "cc5551212"
+            dbCreate = "update"
+            url = "jdbc:mysql://207.223.253.64:3306/cocoaDb"
+            dialect=org.hibernate.dialect.MySQLInnoDBDialect
             properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
+                validationQuery="select 1"
+                testWhileIdle=true
+                timeBetweenEvictionRunsMillis=60000
             }
+
         }
     }
 }
